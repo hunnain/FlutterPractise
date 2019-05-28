13df8544bb4468afa1941cb515ae1d1c30517b8d
Hunnain Pasha <hunnainpashahgchgc@gmail.com>
2019-05-28 22:33:26 +0500

    Update UI1

--- UI1/app1/lib/main.dart
+++ UI1/app1/lib/main.dart
@@ -75,10 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
         backgroundColor: Colors.white,
         leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.arrow_back),
-          color: Colors.grey
-        ),
+            onPressed: () {}, icon: Icon(Icons.arrow_back), color: Colors.grey),
         actions: <Widget>[
           IconButton(
             onPressed: () {},
@@ -103,11 +100,179 @@ class _MyHomePageState extends State<MyHomePage> {
                     fontSize: 20.0,
                   ),
                 ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(25.0, 90.0, 25.0, 0.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 2.0,
+                        color: Colors.grey,
+                      )
+                    ]),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(25.0, 25.0, 5.0, 5.0),
+                          child: Text(
+                            'You Have',
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontFamily: 'Quicksand',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14.0),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(25.0, 40.0, 5.0, 25.0),
+                          child: Text(
+                            '206',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'Quicksand',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 35.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 100.0,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 125.0,
+                      decoration: BoxDecoration(
+                          color: Colors.greenAccent[100].withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Center(
+                        child: Text(
+                          'Buy More',
+                          style: TextStyle(
+                              fontFamily: 'Quicksand',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 40.0),
+          Container(
+            padding: EdgeInsets.only(left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'MY COACHES',
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontFamily: 'Quicksand',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12.0),
+                ),
+                Text(
+                  'VIEW PAST SESSIONS',
+                  style: TextStyle(
+                      color: Colors.green,
+                      fontFamily: 'Quicksand',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12.0),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 2.0,
+            mainAxisSpacing: 4.0,
+            shrinkWrap: true,
+            children: <Widget>[
+              _buildCard('Tony', 'Available', 1),
+              _buildCard('Tony', 'Away', 2),
+              _buildCard('Tony', 'Away', 3),
+              _buildCard('Tony', 'Available', 4),
+              _buildCard('Tony', 'Away', 5),
+              _buildCard('Tony', 'Available', 6),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCard(String name, String status, int cardIndex) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 7.0,
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 12.0,
+          ),
+          Stack(
+            children: <Widget>[
+              Container(
+                height: 60.0,
+                width: 60.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.green,
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            'https://static1.cbrimages.com/wordpress/wp-content/uploads/2019/04/tony-stark-header.jpg'))),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Text(
+                status,
+                style: TextStyle(
+                    fontFamily: 'Quicksand',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12.0,
+                    color: Colors.grey),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Expanded(
+                child: Container(
+                  width: 175.0,
+                  decoration: BoxDecoration(
+                      color: status == 'Away' ? Colors.grey : Colors.green,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10.0),
+                          bottomRight: Radius.circular(10.0))),
+                  child: Center(
+                    child: Text(
+                      'Request',
+                      style: TextStyle(
+                          color: Colors.white, fontFamily: 'Quicksand'),
+                    ),
+                  ),
+                ),
               )
             ],
           )
         ],
       ),
+      margin: cardIndex.isEven
+          ? EdgeInsets.fromLTRB(10.0, 0.0, 25.0, 10.0)
+          : EdgeInsets.fromLTRB(25.0, 0.0, 5.0, 10.0),
     );
   }
 }
