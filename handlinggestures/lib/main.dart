4c657acb418d2d96fc077efe6f0b96f6edbbf542
Hunnain Pasha <hunnainpashahgchgc@gmail.com>
2019-06-19 04:55:22 +0500

    Handling Gesturs

---
+++ handlinggestures/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyButton(),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: (){
+        print('My Button was Pressed');
+      },
+      child: Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500]
+        ),
+        child: Center(
+          child: Text('Engage'),
+        ),
+      ),
+    );
+  }
+}
