e25943383742d71d7ef749f148521298b4966b1a
Hunnain Pasha <hunnainpashahgchgc@gmail.com>
2019-07-02 01:06:51 +0500

    Rating work

---
+++ flutterstatelesswidgetUI/simplerowrating/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  bool _star1 = false;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            GestureDetector(onTap: (){print('Pressed1');},child: Icon(Icons.star, color: Colors.green[500],),),
+            GestureDetector(onTap: (){print('Pressed2');},child: Icon(Icons.star, color: Colors.green[500],),),
+            GestureDetector(onTap: (){print('Pressed3');},child: Icon(Icons.star, color: Colors.green[500],),),
+            GestureDetector(onTap: (){print('Pressed4');},child: Icon(Icons.star, color: Colors.green[500],),),
+            GestureDetector(onTap: (){print('Pressed5');},child: Icon(Icons.star, color: Colors.green[500],),),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
